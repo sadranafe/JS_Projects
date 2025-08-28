51d7e4c03087629e39b693543f413a9d73b3b569
mohammad sadra nafe <sadranafe7@gmail.com>
2025-08-28 12:38:34 +0330

    Add initial variables to JS file

---
+++ otp-generator/script.js
@@ -0,0 +1,10 @@
+const OTPplaceholder = document.querySelector('.OTPplaceholder>span');
+const modalBoxOverlay = document.querySelector('.modalBoxOverlay');
+const modalBox = document.querySelector('.modalBox');
+const modalBoxTitle = document.querySelector('.modalBox>h2');
+const modalBoxContent = document.querySelector('.modalBox>p');
+const otpBtn = document.querySelector('.otpBtnHandler');
+
+const inputs = document.querySelectorAll('input');
+const maxLengthInput = 1;
+const EnteredOTP = [];
