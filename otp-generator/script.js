c62975548d65dd5da2b64a66fedd251c3bf174a2
mohammad sadra nafe <sadranafe7@gmail.com>
2025-08-28 13:07:06 +0330

    feat : Add the OTPgenerator function to the JS file and add it dynamically to the HTML file.

--- otp-generator/script.js
+++ otp-generator/script.js
@@ -8,3 +8,12 @@ const otpBtn = document.querySelector('.otpBtnHandler');
 const inputs = document.querySelectorAll('input');
 const maxLengthInput = 1;
 const EnteredOTP = [];
+
+const OTPgenerator = (min = 5 , max = 6) => {
+    return Math.pow(10 , min) + Math.floor(Math.random() * (Math.pow(10 , max - 1)))
+}
+
+window.addEventListener('DOMContentLoaded' , () => {
+    OTP = OTPgenerator()
+    OTPplaceholder.innerHTML = OTP
+})
