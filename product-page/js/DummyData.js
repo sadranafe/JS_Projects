811dc0206b34e722d4b9a61758853fd635950349
mohammad sadra nafe <sadranafe7@gmail.com>
2025-10-17 23:53:46 +0330

    feat: add the Dummy Data to load the products dynamically

---
+++ product-page/js/DummyData.js
@@ -0,0 +1,67 @@
+const productsData = [
+  {
+    id: 1,
+    name: "airpods",
+    category: "accessories",
+    price: 1200,
+    img: "img/airpods.png",
+  },
+  {
+    id: 2,
+    name: "bag",
+    category: "cloth",
+    price: 730,
+    img: "img/bag.png",
+  },
+  {
+    id: 3,
+    name: "cream",
+    category: "health",
+    price: 1239,
+    img: "img/cream.png",
+  },
+  {
+    id: 4,
+    name: "iphone 14 promax",
+    category: "mobile",
+    price: 2099,
+    img: "img/iphone.png",
+  },
+  {
+    id: 5,
+    name: "jacket",
+    category: "cloth",
+    price: 250,
+    img: "img/jacket.png",
+  },
+  {
+    id: 6,
+    name: "shampoo",
+    category: "health",
+    price: 250,
+    img: "img/shampoo.png",
+  },
+  {
+    id: 7,
+    name: "shoes",
+    category: "cloth",
+    price: 476,
+    img: "img/shoes.png",
+  },
+  {
+    id: 8,
+    name: "vitamin",
+    category: "health",
+    price: 100,
+    img: "img/vitamin.png",
+  },
+  {
+    id: 9,
+    name: "watch",
+    category: "accessories",
+    price: 700,
+    img: "img/watch.png",
+  },
+];
+
+export default productsData;
